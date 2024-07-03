6856ac5fe399650ca3824b19741f165855e60322
minq <james2224@hanmail.net>
2024-07-03 14:12:48 +0900

    [feature] Page27

--- Testable/test.cpp
+++ Testable/test.cpp
@@ -1,7 +1,12 @@
+#include <stdexcept>
+
+using namespace std;
+
 #include "pch.h"
 #include "../Baseball/Baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched)
+{
+    Baseball game;
+    EXPECT_THROW(game.guess(string("12")), length_error);
 }
