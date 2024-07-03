bdb38364ec3597f1e09a966debeb6c0887c43b26
minq <james2224@hanmail.net>
2024-07-03 14:23:04 +0900

    [feature] Page42

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -7,6 +7,11 @@ class Baseball
 public:
     void guess (const string& guessNumber)
     {
+        assertIllegalArgument(guessNumber);
+    }
+
+    void assertIllegalArgument(const std::string& guessNumber)
+    {
         if (guessNumber.length() != 3)
         {
             throw length_error("Must be three letters.");
@@ -14,17 +19,19 @@ public:
 
         for (char c : guessNumber)
         {
-            if (c < '0' || c > '9')
-            {
-                throw invalid_argument("Must be number.");
-            }
+            if (c >= '0' && c <= '9') continue;
+            throw invalid_argument("Must be number.");
         }
 
-        if ((guessNumber[0] == guessNumber[1]) ||
-            (guessNumber[0] == guessNumber[2]) ||
-            (guessNumber[1] == guessNumber[2]))
+        if (isDuplicatedNumber(guessNumber))
         {
             throw invalid_argument("Must not have the same number.");
         }
+    }
+    bool isDuplicatedNumber(const std::string& guessNumber)
+    {
+        return (guessNumber[0] == guessNumber[1]) ||
+            (guessNumber[0] == guessNumber[2]) ||
+            (guessNumber[1] == guessNumber[2]);
     }
 };
